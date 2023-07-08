30df9713ce2a7a810bba87e5e896105dbb213416
Luis_Miguel_Gonzalez_Giraldo <miguel.gonzalez@cinnco.co>
2023-07-08 08:30:13 -0500

    Se finaliza el CRUD de libros

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -30,4 +30,48 @@ class BookController extends Controller
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+    public function show($id)
+    {
+        $book = Book::find($id);
+
+        if (empty($book)) {
+            return response()->json(['success' => false, 'message' => 'El libro no existe.'], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json(['success' => true, 'data' => $book]);
+    }
+
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+
+        return response()->json(['success' => true, 'message' => 'Libro creado con éxito', 'data' => $book]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $book = Book::find($id);
+
+        if (empty($book)) {
+            return response()->json(['success' => false, 'message' => 'El libro no existe.'], Response::HTTP_NOT_FOUND);
+        }
+
+        $book->update($request->all());
+
+        return response()->json(['success' => true, 'message' => 'Libro actualizado con éxito.', 'data' => $book]);
+    }
+
+    public function destroy($id)
+    {
+        $book = Book::find($id);
+
+        if (empty($book)) {
+            return response()->json(['success' => false, 'message' => 'El libro no existe.'], Response::HTTP_NOT_FOUND);
+        }
+
+        $book->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
 }
